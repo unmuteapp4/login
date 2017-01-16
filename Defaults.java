83d68462122c897937b576d4145a95c1ee1816e5
abhishek <unmuteapp4@gmail.com>
2017-01-16 17:52:25 +0530

    Initial commit

---
+++ Defaults.java
@@ -0,0 +1,9 @@
+package com.backendless.unmute.login;
+
+public class Defaults
+{
+  public static final String APPLICATION_ID = "EE4292B6-4090-1756-FFD1-CE1B77EE0B00";
+  public static final String SECRET_KEY = "90A94C70-E186-CFD3-FFC0-F8C7B4C39C00";
+  public static final String VERSION = "v1";
+  public static final String SERVER_URL = "https://api.backendless.com";
+}
